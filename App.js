78192915d0a0993e91cd7c5d46dbfeefaf34f762
GeovannaGame1YT <90795502+GeovannaGame1YT@users.noreply.github.com>
2022-06-27 12:01:35 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,27 +1,25 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, SafeAreaView, useEffect, Text, View, Button, TouchableOpacity, Alert } from 'react-native';
-import { NativeAppEventEmitter, navigator } from 'react-native';
-import TelaReturn from './componentes/TelaReturn';
-import Cores from './componentes/Tetste';
+//import { NativeAppEventEmitter, navigator } from 'react-native';
+import Botao from './componentes/Cartas';
 import TelaPrincipal from './componentes/TelaIs';
-import { render } from 'react-dom';
 
-function onPress() {
-  NativeAppEventEmitter.TelaPrincipal;
+//este sistema foi feita com base em um jogo de carata e uma tabela
+//grid do proprio tutorial do react ^-^
+function precionado() {
 }
+
 export default function App() {
-  render()
   return (
     <>
-    <View style={styles.container}>
-      <Text>Jogo Da Memoria</Text>
-      <StatusBar style="auto" />
-      <View style={styles.main}>
-                <Button titulo="Play Game" precionado={onPress}/>
-            </View>
-    </View>
-    <TelaReturn/> //não esta mais vindo esta tela
+    <SafeAreaView style={styles.container}>
+      <Botao/>
+          <TelaPrincipal/>
+            {
+                
+            }
+   </SafeAreaView>
     </>
   );
 }
@@ -35,5 +33,5 @@ const styles = StyleSheet.create({
   main: {
     flex: 1,
     justifyContent: 'flex-end'
-  }
+  },
 });
